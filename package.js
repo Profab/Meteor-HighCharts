52c8b9d77746481cdbb566c17b8d7ba4421cb7fb
Richard Lai <it.rclai@gmail.com>
2015-01-28 14:30:10 +0000

    Highjacked

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     summary: "HighCharts for Meteor, with an easy to use helper to get you started!",
     version: "0.2.3",
-    git: "https://github.com/MaazAli/Meteor-HighCharts.git"
+    git: "https://github.com/Profab/Meteor-HighCharts.git"
 });
 
 Package.onUse(function (api) {
